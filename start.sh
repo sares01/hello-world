c9adfe3328f1362fd90ebb71bb5eaceac2f6901f
sares01 <salar.reshu@outlook.de>
2022-04-13 16:22:53 +0200

    sssss

--- start.sh
+++ start.sh
@@ -8,5 +8,4 @@ echo "APP_S" > secret
 
 base64 -d secret > app_secrets
 cat app_secrets
-
- #gauge run -s -v --simple-console --fail-safe --tags "$IGNORE_TAGS" --env "$ENV_UMGEBUNG" specs/
+#gauge run -s -v --simple-console --fail-safe --tags "$IGNORE_TAGS" --env "$ENV_UMGEBUNG" specs/
